13de697cd4064fe35a3accda067e49127f470e57
Igustiawan <igustiawan46@gmail.com>
2025-02-14 16:47:39 +0700

    Frontend master bucket

---
+++ Minio/Controllers/BucketController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Minio.Models;
+using Minio.Services;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+
+namespace Minio.Controllers
+{
+    public class BucketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
