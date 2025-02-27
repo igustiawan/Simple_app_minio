c183c964f06c9db5b93cc7262493a39c23cbf1ef
Igustiawan <igustiawan46@gmail.com>
2025-02-27 13:34:23 +0700

    update my files get data from minio api

--- Minio/Controllers/MyFilesController.cs
+++ Minio/Controllers/MyFilesController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Minio.Services;
 
 namespace Minio.Controllers
 {
     public class MyFilesController : Controller
     {
+        private readonly FileUploadService _uploadService;
+        private static Dictionary<string, double> _progressTracker = new Dictionary<string, double>();
+
+        public MyFilesController(FileUploadService uploadService)
+        {
+            _uploadService = uploadService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFileList()
+        {
+            try
+            {
+                var fileList = await _uploadService.GetFileListAsync();
+                return Json(fileList);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = $"Error: {ex.Message}" });
+            }
         }
     }
 }
